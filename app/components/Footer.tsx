5f8e7cdf105d915ba13d33b798c5d0014887e820
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2024-10-02 18:32:27 +0200

    responsive footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,34 +10,36 @@ import swissBerry from '@/public/svg/swiss-berry.svg';
 export default function About() {
     return (
         <>
-            <footer className="w-full flex flex-wrap h-64 border-t-2 items-center mt-96">
-                <h2 className={`${bodoni_moda.className} text-[40px] w-1/2 text-center border-r-[3px] px-4`}>MARIUS OLIVIER</h2>
-                <div className="w-1/2 flex flex-wrap space-x-14 px-4 justify-center text-sm" >
-                    <div className='flex flex-col space-y-2'>
-                        <div className='flex flex-wrap space-x-4 list-none'>
-                        {links.map(link => (
-                            <li key={link.href} className="relative">
-                                <Link href={link.href} >
-                                    {link.label}
-                                </Link>
-                            </li>
-                        ))}
+            <footer className="flex flex-col p-10">
+                <div className=' w-full flex flex-col md:flex-row pt-32 py-10 border-t-2 items-center mt-96'>
+                    <h2 className={`${bodoni_moda.className} text-[40px] md:w-1/2 text-center md:border-r-[3px] px-4`}>MARIUS OLIVIER</h2>
+                    <div className="w-1/2 flex flex-wrap md:space-x-14 md:px-4 justify-center text-sm" >
+                        <div className='flex flex-col space-y-2'>
+                            <div className='flex flex-col space-y-6 md:space-y-0 md:flex-row items-center md:space-x-4 list-none text-md p-6 md:p-0'>
+                                {links.map(link => (
+                                    <li key={link.href} className="relative">
+                                        <Link href={link.href} >
+                                            {link.label}
+                                        </Link>
+                                    </li>
+                                ))}
 
+                                </div>
+                            <p className='p-6 md:p-0'>2024 - Tous droits réservés</p>
                         </div>
-                        <p>2024 - Tous droits réservé</p>
-                    </div>
-                    <div className='flex flex-wrap  space-x-5 items-center '>
-                        <a className='und underline decoration-[#8A1D1D] underline-offset-4 decoration-2' href="mailto:contact@marius-ollivier.ch">contact@marius-ollivier.ch</a>
-                        <a className='bg-gradient-radial from-[#8A1D1D] to-[#884F4F] rounded-md p-1' href="https://www.instagram.com/p/C27u1PPMJKm/" target="_blank">
-                            <Image
-                            priority
-                            src={Instagram}
-                            alt="Arrow icon"
-                            />
-                        </a>
+                        <div className='flex flex-col space-y-6 md:space-y-6 md:flex-row md:space-x-5 items-center'>
+                            <a className='underline decoration-[#8A1D1D] underline-offset-4 decoration-2' href="mailto:contact@marius-ollivier.ch">contact@marius-ollivier.ch</a>
+                            <a className='bg-gradient-radial from-[#8A1D1D] to-[#884F4F] rounded-md p-1' href="https://www.instagram.com/p/C27u1PPMJKm/" target="_blank">
+                                <Image
+                                priority
+                                src={Instagram}
+                                alt="Arrow icon"
+                                />
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <a className="flex w-full justify-center space-x-4 items-center" href="https://www.swiss-berry.com" target="_blank">
+                <a className=" flex w-full justify-center space-x-4 items-center" href="https://www.swiss-berry.com" target="_blank">
                     <p className="text-gray-50">Website made by</p>
                     <Image
                     priority
