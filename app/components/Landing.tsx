02b55810fbbe24194e4eea63bd80a4a7de636b0e
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2024-10-05 16:08:22 +0200

    fix responsive landing page

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -6,76 +6,74 @@ import { bodoni_moda } from '../fonts/fonts'
 
 export function Landing() {
   return (
-    <div
-      className={cn(
-        "relative flex min-h-[950px] flex-col items-center justify-center overflow-hidden w-full -z-20 -mt-48"
-      )}
-    >
-      <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0">
-        <motion.div
-          initial={{ opacity: 0.5, width: "15rem" }}
-          whileInView={{ opacity: 1, width: "60rem" }}
-          transition={{
-            delay: 0,
-            duration: 1,
-            ease: "easeInOut",
-          }}
-          viewport={{ once: true }}
-          style={{
-            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-          }}
-          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[60rem] bg-gradient-conic from-red-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
-        >
-          <div className="absolute w-[100%] left-0 bg-[#171717] h-40 bottom-0 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute w-40 h-[100%] left-0 bg-[#171717] bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0.5, width: "15rem" }}
-          whileInView={{ opacity: 1, width: "60rem" }}
-          transition={{
-            delay: 0,
-            duration: 1,
-            ease: "easeInOut",
-          }}
-          viewport={{ once: true }}
-          style={{
-            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
-          }}
-          className="absolute inset-auto left-1/2 h-56 w-[60rem] bg-gradient-conic from-transparent via-transparent to-red-500 text-white [--conic-position:from_290deg_at_center_top]"
-        >
-          <div className="absolute w-40 h-[100%] right-0 bg-[#171717] bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div className="absolute w-[100%] right-0 bg-[#171717] h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-        </motion.div>
-        <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-[#171717] blur-2xl"></div>
-        <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-red-500 opacity-50 blur-3xl"></div>
-        <motion.div
-          initial={{ width: "8rem" }}
-          whileInView={{ width: "16rem" }}
-          transition={{
-            delay: 0,
-            duration: 1,
-            ease: "easeInOut",
-          }}
-          viewport={{ once: true }}
-          className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-red-400 blur-2xl"
-        ></motion.div>
-        <motion.div
-          initial={{ width: "15rem" }}
-          whileInView={{ width: "60rem" }}
-          transition={{
-            delay: 0,
-            duration: 1,
-            ease: "easeInOut",
-          }}
-          viewport={{ once: true }}
-          className="absolute inset-auto z-50 h-0.5 w-[60rem] -translate-y-[7rem] bg-red-400 "
-        ></motion.div>
+    <>
+      <div className="h-screen flex flex-col justify-between">
+        <div className="flex flex-col -space-y-64">
+        <div className={cn("relative flex h-[500px] flex-col items-center justify-center overflow-hidden w-full")}>
+          <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate ">
+            <motion.div
+              initial={{ opacity: 0.5, width: "15rem" }}
+              whileInView={{ opacity: 1, width: "60rem" }}
+              transition={{
+                delay: 0,
+                duration: 1,
+                ease: "easeInOut",
+              }}
+              viewport={{ once: true }}
+              style={{
+                backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+              }}
+              className="absolute inset-auto right-1/2 h-56 overflow-visible w-[60rem] bg-gradient-conic from-red-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+              >
+              <div className="absolute w-[100%] left-0 bg-[#171717] h-40 bottom-0 [mask-image:linear-gradient(to_top,white,transparent)]" />
+              <div className="absolute w-40 h-[100%] left-0 bg-[#171717] bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0.5, width: "15rem" }}
+              whileInView={{ opacity: 1, width: "60rem" }}
+              transition={{
+                delay: 0,
+                duration: 1,
+                ease: "easeInOut",
+              }}
+              viewport={{ once: true }}
+              style={{
+                backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+              }}
+              className="absolute inset-auto left-1/2 h-56 w-[60rem] bg-gradient-conic from-transparent via-transparent to-red-500 text-white [--conic-position:from_290deg_at_center_top]"
+            >
+              <div className="absolute w-40 h-[100%] right-0 bg-[#171717] bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+              <div className="absolute w-[100%] right-0 bg-[#171717] h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+            </motion.div>
+            <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-[#171717] blur-2xl"></div>
+            <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
+            <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-red-500 opacity-50 blur-3xl"></div>
+            <motion.div
+              initial={{ width: "8rem" }}
+              whileInView={{ width: "16rem" }}
+              transition={{
+                delay: 0,
+                duration: 1,
+                ease: "easeInOut",
+              }}
+              viewport={{ once: true }}
+              className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-red-400 blur-2xl"
+            ></motion.div>
+            <motion.div
+              initial={{ width: "15rem" }}
+              whileInView={{ width: "60rem" }}
+              transition={{
+                delay: 0,
+                duration: 1,
+                ease: "easeInOut",
+              }}
+              viewport={{ once: true }}
+              className="absolute inset-auto z-50 h-0.5 w-[60rem] -translate-y-[7rem] bg-red-400 "
+            ></motion.div>
 
-        <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-[#171717]"></div>
-      </div>
-
-      <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
+            <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-[#171717]"></div>
+          </div>
+        </div>
         <motion.h1
           initial={{ opacity: 0, y: 200 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -85,11 +83,14 @@ export function Landing() {
             ease: "easeInOut",
           }}
           viewport={{ once: true }}
-          className={`${bodoni_moda.className} bg-gradient-to-b from-red-400 to-white pb-40 pt-40 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent xl:text-[180px]`}
-        >
+          className={`${bodoni_moda.className} text-center text-[80px] md:text-[105px] lg:text-[140px] xl:text-[160px] 2xl:text-[180px] bg-clip-text text-transparent bg-gradient-to-b from-red-500 to-red-100`}>
           Marius Ollvier
         </motion.h1>
       </div>
-    </div>
+      {/* <div className="flex w-full bg-red-500 justify-end">
+        <h1>test</h1>
+      </div> */}
+      </div>
+    </>
   );
 }
