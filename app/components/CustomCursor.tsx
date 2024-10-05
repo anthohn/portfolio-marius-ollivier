9b97c6e8bcaf4f355e5ac49f69e3979508253d6f
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2024-10-05 15:07:45 +0200

    fix header mobile & add swissflag & remove custom curosor on mobile

--- app/components/CustomCursor.tsx
+++ app/components/CustomCursor.tsx
@@ -1,11 +1,25 @@
-// components/CustomCursor.tsx
 'use client'
+
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+// Fonction pour détecter si l'utilisateur est sur un mobile via le user-agent
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = typeof window !== 'undefined' ? navigator.userAgent : '';
+    const mobileRegex = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/;
+    setIsMobile(mobileRegex.test(userAgent)); // Tester si le user-agent correspond à un appareil mobile
+  }, []);
+
+  return isMobile;
+};
+
 const CustomCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [visible, setVisible] = useState(false); // Pour gérer la visibilité initiale
+  const [visible, setVisible] = useState(false);
+  const isMobile = useIsMobile(); // Utiliser le hook pour détecter le mobile
 
   // Fonction pour mettre à jour la position du curseur
   const handleMouseMove = (e: MouseEvent) => {
@@ -14,6 +28,8 @@ const CustomCursor = () => {
   };
 
   useEffect(() => {
+    if (isMobile) return; // Ne pas activer le custom cursor en mode mobile
+
     // Initialiser à la position courante du curseur
     setPosition({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
     window.addEventListener('mousemove', handleMouseMove);
@@ -21,29 +37,21 @@ const CustomCursor = () => {
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, []);
+  }, [isMobile]); // Dépend de `isMobile`
+
+  // Ne rien rendre si on est sur mobile ou que le curseur n'est pas visible
+  if (isMobile || !visible) return null;
 
   return (
-    visible && (
-      // <motion.div
-      //   className="fixed top-0 left-0 w-4 h-4 bg-blue-500 rounded-full pointer-events-none"
-      //   style={{
-      //     x: position.x - 8, // Ajuster la position pour centrer la boule
-      //     y: position.y - 8,
-      //   }}
-      //   animate={{ x: position.x - 8, y: position.y - 8 }}
-      //   transition={{ type: 'spring', stiffness: 500, damping: 30 }}
-      // />
-      <motion.div
-        className="fixed -top-10 -left-5 w-20 h-20 bg-[#462525] z-10 blur-md rounded-full pointer-events-none"
-        style={{
-              x: position.x - 8, // Ajuster la position pour centrer la boule
-              y: position.y - 8,
-            }}
-        animate={{ x: position.x - 8, y: position.y - 8 }}
-        transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-      />
-    )
+    <motion.div
+      className="fixed -top-10 -left-5 w-20 h-20 bg-[#462525] z-10 blur-md rounded-full pointer-events-none"
+      style={{
+        x: position.x - 8, // Ajuster la position pour centrer la boule
+        y: position.y - 8,
+      }}
+      animate={{ x: position.x - 20, y: position.y - 0 }}
+      transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+    />
   );
 };
 
